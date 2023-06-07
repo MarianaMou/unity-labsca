fcb518ac101ab3c859fc78e7f724b8c1d1fd9170
MarianaMou <64329333+MarianaMou@users.noreply.github.com>
2023-06-07 17:07:14 +0200

    A toi

--- Lab/Assets/script/sortie.cs
+++ Lab/Assets/script/sortie.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sortie : MonoBehaviour
 {
     public Light light1;
     public Light light2;
     public Light light3;
+    public GameObject mur;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,7 +21,8 @@ public class sortie : MonoBehaviour
     {
         if (light1.color == Color.green && light2.color == Color.green && light3.color == Color.green) {
            Debug.Log("Tu peux sortir");
-           Destroy(gameObject);
+           Destroy(mur);
+           anim.SetBool("Fini",true);
         }
     }
 }
